9538c713ec8bdf6656acf0422155209db78f9ba4
Lucas Lalumière <111912000+Shlucus@users.noreply.github.com>
2025-05-29 20:18:13 -0400

    Update payload.js

--- javascript/payload.js
+++ javascript/payload.js
@@ -1,2 +1,2 @@
-document.location = "https://st.depositphotos.com/1014014/2679/i/950/depositphotos_26796645-stock-photo-you-have-been-hacked-sign.jpg";
+document.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&themeRefresh=1";
 alert("Payload executed");
