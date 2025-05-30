f5c4d24937e5bdca462451ee86dc843658624db1
Lucas Lalumière <111912000+Shlucus@users.noreply.github.com>
2025-05-29 20:10:00 -0400

    Update payload.js

--- javascript/payload.js
+++ javascript/payload.js
@@ -1,1 +1,1 @@
-alert("Payload loaded! Document cookies: " + document.cookie);
+document.location = "https://st.depositphotos.com/1014014/2679/i/950/depositphotos_26796645-stock-photo-you-have-been-hacked-sign.jpg"
