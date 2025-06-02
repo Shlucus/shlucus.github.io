52898b32624ab7b48e6bb3a34ea70b5e497ddbeb
Lucas Lalumière <111912000+Shlucus@users.noreply.github.com>
2025-06-02 16:23:28 -0400

    Update payload.js

--- javascript/payload.js
+++ javascript/payload.js
@@ -1,2 +1,2 @@
-// document.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&themeRefresh=1";
+document.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&themeRefresh=1";
 alert("Payload executed");
